4c7649b27d537a5f6f877bda3f4df6b302196bff
Hyunwoo Nam <namenu@gmail.com>
2021-07-06 23:22:45 +0900

    deps -> lein

--- project.clj
+++ project.clj
@@ -1,5 +1,3 @@
 ;; Netlify - leiningen 배포에 필요
 (defproject namenu/maze "0.1.0-SNAPSHOT"
-  :dependencies [[thheller/shadow-cljs "2.14.5" :classifier "aot"]
-                 [org.clojure/core.async "1.3.618"]
-                 [reagent/reagent "1.0.0"]])
+  :dependencies [[thheller/shadow-cljs "2.14.5" :classifier "aot"]])
